6191a19b2f1aa07af311f8105c910970df2d6459
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-09-10 12:59:29 +0530

    update_profile

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
 
         if (user) {
           localStorage.setItem("uid", user._id);
-          alert("Login completed successfully , welcome :)");
+          alert("Login completed successfully. Welcome!");
           navigate("/Home");
           setTimeout(() => {
             window.location.reload();
